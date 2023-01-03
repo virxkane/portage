d03d8047da34f88319ba1c1140b4bbbbd54ab827
Aleksey Chernov <valexlin@gmail.com>
2023-01-03 18:14:38 +0400

    Massive cleanup: remove old

--- gui-libs/gdk-pixbuf/files/postinstall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-PREFIX="/mingw"
-
-echo "enum gdk-pixbuf-loader"
-mkdir -p ${PREFIX}/etc/gtk-2.0
-LANG=en ${PREFIX}/bin/gdk-pixbuf-query-loaders > "${PREFIX}/lib/gdk-pixbuf-2.0/2.10.0/loaders.cache"
